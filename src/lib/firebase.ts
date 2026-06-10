33e97a7c31e8f28e76bb9fad434fad2a4a1764ba
Vatsal Dhuvad <vatsaldhuvad23@gmail.com>
2026-06-10 22:17:33 +0530

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAvnwXrfGMhx002ZL3uBTwbpvQyx5J7vkU",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, { experimentalForceLongPolling: true });
