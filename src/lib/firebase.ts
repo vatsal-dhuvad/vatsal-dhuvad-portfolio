26ce7aade7f527391ae42e7f5960e4cdeb38992b
Vatsal Dhuvad <vatsaldhuvad23@gmail.com>
2026-06-18 15:48:53 +0530

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,9 +2,10 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvnwXrfGMhx002ZL3uBTwbpvQyx5J7vkU",
+  apiKey: "AIzaSyAvnwXrFgMHx002ZL3uBTwbpvQyx5J7vkU",
   authDomain: "vatsal-portfolio-5699f.firebaseapp.com",
   projectId: "vatsal-portfolio-5699f",
+  storageBucket: "vatsal-portfolio-5699f.firebasestorage.app",
   messagingSenderId: "794035958468",
   appId: "1:794035958468:web:95a40e905c18ebec6f8f91",
   measurementId: "G-P2M1V2TBTV"
